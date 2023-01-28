f5c76f76f0ff8ef96f11d4e688c303e006ec5fbd
Raphael Lima <phaelslima@gmail.com>
2023-01-28 13:26:44 -0300

    feat(mobile): add home screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,6 @@
 import './src/lib/dayjs';
 
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 import {
   useFonts,
   Inter_400Regular,
@@ -10,6 +10,7 @@ import {
 } from '@expo-google-fonts/inter';
 
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,22 +23,9 @@ export default function App() {
   if (!fontsLoaded) return <Loading />;
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+    <>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontFamily: 'Inter_400Regular',
-    color: '#FFFFFF'
-  }
-});
